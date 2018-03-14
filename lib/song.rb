f3da4958d7214e097a5e676789fe7a1d733cf0cb
Holden Altaffer <haltaffer@gmail.com>
2018-03-14 00:14:07 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -19,6 +19,7 @@ class Song
     #   self.artist.name = name
     # end
     self.artist = Artist.find_or_create_by_name(name)
+    self.artist.add_song(self)
   end
 
   def self.new_by_filename(file_name)
